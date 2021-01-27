113596e73eec4c4e0f65013c0a1bcb108461bd52
Gaurav Menezes <gauravmenezes@gmail.com>
2021-01-27 20:22:44 +0530

    Updated CORS to include host

--- src/main/java/com/program/qga/config/WebConfig.java
+++ src/main/java/com/program/qga/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://cga-frontend.herokuapp.com/");
+        registry.addMapping("/**").allowedOrigins("https://cga-frontend.herokuapp.com");
     }
 }
